8896474a722fbece7ba2371afbceb4d5b75810a0
YU WEI <vic.weiyu@gmail.com>
2021-12-13 11:40:11 +1030

    R3 - 20211213

--- src/07-event-emitter.js
+++ src/07-event-emitter.js
@@ -1,4 +1,6 @@
 const EventEmitter = require('events');
+
+/*
 const em = new EventEmitter();
 em.on('tigger', (p, ...others) => {
   console.log(`on tigger: p:${p}, others:${others}`);
@@ -9,6 +11,24 @@ const timerID = setInterval(() => {
   em.emit('tigger', 'Hello', 'NodeJS', 'Express', 'Koa', 'NestJS');
   count++;
   if (count >= 5) {
-    clearTimeout(timerID);
+    clearInterval(timerID);
+  }
+}, 1000);
+*/
+
+console.log('********************');
+
+const em2 = new EventEmitter();
+em2.once('tiggerOnce', (p) => {
+  console.log('once', p);
+});
+
+let count2 = 0;
+const timerID2 = setInterval(() => {
+  console.log('Interval', count2);
+  em2.emit('tiggerOnce', count2);
+  count2++;
+  if (count2 >= 5) {
+    clearInterval(timerID2);
   }
 }, 1000);
